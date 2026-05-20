1589be68eecc4adb5f5b658b8a755a5f4bdbef07
Khushi Kumari <khushikumari240824@gmail.com>
2026-05-21 01:06:31 +0530

    socket.services

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,23 @@
-const app = require("./app");
-const { connectDatabase } = require("./config/db");
-const env = require("./config/env");
+const app = require('./app');
+const { connectDatabase } = require('./config/db');
+const env = require('./config/env');
+const http = require('http');
+const { initSocket } = require('./services/socket.service');
 
 const startServer = async () => {
   try {
     await connectDatabase();
-    app.listen(env.port, () => {
+
+    const server = http.createServer(app);
+
+    // initialize Socket.io and attach to the server
+    initSocket(server);
+
+    server.listen(env.port, () => {
       console.log(`Server running on port ${env.port}`);
     });
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
