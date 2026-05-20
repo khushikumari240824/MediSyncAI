6c99fb18b76ba5cd03b3900dd490bde975ce35b4
Khushi Kumari <khushikumari240824@gmail.com>
2026-05-21 00:59:35 +0530

    user.js

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
     email: {
       type: String,
       required: true,
@@ -14,34 +19,63 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       minlength: 6,
+      select: false,
     },
     role: {
       type: String,
-      enum: ["patient", "doctor", "hospital"],
+      enum: ['admin', 'doctor', 'patient', 'receptionist', 'labTechnician'],
       required: true,
     },
-    profileId: {
-      type: mongoose.Schema.Types.ObjectId,
-      refPath: "roleModel",
-    },
-    roleModel: {
+    profileImage: {
       type: String,
-      enum: ["Patient", "Doctor", "Hospital"],
+      default: '',
+    },
+    phoneNumber: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female', 'other'],
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
     },
   },
   {
     timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
   },
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.models.User || mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
