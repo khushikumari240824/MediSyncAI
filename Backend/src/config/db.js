5291c0c818f8f077885989bf28c02308469feeb8
Khushi Kumari <khushikumari240824@gmail.com>
2026-05-21 12:19:09 +0530

    remove duplicate root project

--- Backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDatabase = async () => {
-  const env = require("./env");
-  await mongoose.connect(env.mongoUri);
-  console.log("MongoDB Connected");
-};
-
-module.exports = { connectDatabase };
--- Frontend/src/services/authService.js
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const authService = {
-  login: (email, password) => api.post('/auth/login', { email, password }),
-  getCurrentUser: () => api.get('/auth/me'),
-  register: (role, data) => api.post(`/auth/register/${role}`, data),
-  resetPassword: (email, newPassword) => api.post('/auth/reset-password', { email, newPassword })
-};
